33fac28176eb8d6ab239f85c1d0c31ea4359550b
Iveta <iveta.gencheva@rwth-aachen.de>
2026-02-28 13:49:21 +0100

    first add

--- app.js
+++ app.js
@@ -2,3 +2,23 @@ function start() {
     window.location.href = 'start.html';
     return false;    
 }
+
+function enter(ele) {
+    if(event.key === 'Enter') {
+        document.getElementById('debug-field').innerHTML = "Pressed!";
+    }
+}
+
+function testingfunc(ele) {
+    pardiv = ele.parentElement.parentElement;
+    inpfields = pardiv.children;
+
+    // val = ele.value;
+
+    // if (ele.value != "") {
+    //     ele.value = "";
+    // }
+
+    inpfields[3].children[0].focus();
+    // inpfields[2].children[0].value = val;
+}
